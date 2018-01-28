5d745faf3a011681f5760e89ead32ee6f633b090
MajdAldenH <majdhi1995@hotmail.com>
2018-01-28 23:05:33 +0200

    Update MultiContactPicker.java

--- multicontactpicker/src/main/java/com/wafflecopter/multicontactpicker/MultiContactPicker.java
+++ multicontactpicker/src/main/java/com/wafflecopter/multicontactpicker/MultiContactPicker.java
@@ -1,7 +1,7 @@
 package com.wafflecopter.multicontactpicker;
 
 import android.app.Activity;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
